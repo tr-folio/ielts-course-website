7d889c494d7932ea35407bf801f3888320725c2f
tr-folio <tahrimurmoon@gmail.com>
2021-10-04 12:38:23 +0600

    react bootstrap is working and added with header navigation

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,17 +1,78 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
+
 
 const Header = () => {
     return (
         <div className="d-block">
-            <div>
-                <nav>
-                    <NavLink to="/home">Home</NavLink>
-                    <NavLink to="/about">About</NavLink>
-                    <NavLink to="/services">Services</NavLink>
-                    <NavLink to="/notices">Notices</NavLink>
-                </nav>
-            </div>
+            {/* <Navbar bg="dark" expand="lg" className="px-3">
+                <Navbar.Brand className="text-info">IELTS Course</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                    className="mr-auto my-2 my-lg-0"
+                    style={{ maxHeight: '100px' }}
+                    navbarScroll
+                    >
+                        
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar> */}
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand className="text-info">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <NavLink 
+                                to="/home" 
+                                className="text-light"
+                                style={{
+                                    textDecoration: "none",
+                                    padding: "10px"
+                                }}
+                                activeStyle={{
+                                    fontWeight: "bold"
+                                }}
+                            >Home</NavLink>
+                            <NavLink 
+                                to="/about" 
+                                className="text-light"
+                                style={{
+                                    textDecoration: "none",
+                                    padding: "10px"
+                                }}
+                                activeStyle={{
+                                    fontWeight: "bold"
+                                }}
+                            >About</NavLink>
+                            <NavLink 
+                                to="/services" 
+                                className="text-light"
+                                style={{
+                                    textDecoration: "none",
+                                    padding: "10px"
+                                }}
+                                activeStyle={{
+                                    fontWeight: "bold"
+                                }}
+                            >Services</NavLink>
+                            <NavLink 
+                                to="/notices" 
+                                className="text-light"
+                                style={{
+                                    textDecoration: "none",
+                                    padding: "10px"
+                                }}
+                                activeStyle={{
+                                    fontWeight: "bold"
+                                }}
+                            >Notices</NavLink>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
