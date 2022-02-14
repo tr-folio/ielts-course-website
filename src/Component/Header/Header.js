dcc42955f38a456c245f175ec383e42fb076e118
tr-folio <tahrimurmoon@gmail.com>
2022-02-14 17:53:12 +0600

    updated main services ui

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -6,71 +6,58 @@ import { NavLink } from 'react-router-dom';
 const Header = () => {
     return (
         <div className="d-block">
-            {/* <Navbar bg="dark" expand="lg" className="px-3">
-                <Navbar.Brand className="text-info">IELTS Course</Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarScroll" />
-                <Navbar.Collapse id="navbarScroll">
-                    <Nav
-                    className="mr-auto my-2 my-lg-0"
-                    style={{ maxHeight: '100px' }}
-                    navbarScroll
-                    >
-                        
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar> */}
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand className="text-info">IELTS-Course</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="me-auto">
-                            <NavLink 
-                                to="/home" 
-                                className="text-light"
-                                style={{
-                                    textDecoration: "none",
-                                    padding: "10px"
-                                }}
-                                activeStyle={{
-                                    fontWeight: "bold"
-                                }}
-                            >Home</NavLink>
-                            <NavLink 
-                                to="/about" 
-                                className="text-light"
-                                style={{
-                                    textDecoration: "none",
-                                    padding: "10px"
-                                }}
-                                activeStyle={{
-                                    fontWeight: "bold"
-                                }}
-                            >About</NavLink>
-                            <NavLink 
-                                to="/services" 
-                                className="text-light"
-                                style={{
-                                    textDecoration: "none",
-                                    padding: "10px"
-                                }}
-                                activeStyle={{
-                                    fontWeight: "bold"
-                                }}
-                            >Services</NavLink>
-                            <NavLink 
-                                to="/notices" 
-                                className="text-light"
-                                style={{
-                                    textDecoration: "none",
-                                    padding: "10px"
-                                }}
-                                activeStyle={{
-                                    fontWeight: "bold"
-                                }}
-                            >Notices</NavLink>
-                        </Nav>
-                    </Navbar.Collapse>
+                        <Navbar.Brand className="text-info">IELTS-Course</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className="me-auto">
+                                <NavLink 
+                                    to="/home" 
+                                    className="text-light"
+                                    style={{
+                                        textDecoration: "none",
+                                        padding: "10px"
+                                    }}
+                                    activeStyle={{
+                                        fontWeight: "bold"
+                                    }}
+                                >Home</NavLink>
+                                <NavLink 
+                                    to="/about" 
+                                    className="text-light"
+                                    style={{
+                                        textDecoration: "none",
+                                        padding: "10px"
+                                    }}
+                                    activeStyle={{
+                                        fontWeight: "bold"
+                                    }}
+                                >About</NavLink>
+                                <NavLink 
+                                    to="/services" 
+                                    className="text-light"
+                                    style={{
+                                        textDecoration: "none",
+                                        padding: "10px"
+                                    }}
+                                    activeStyle={{
+                                        fontWeight: "bold"
+                                    }}
+                                >Services</NavLink>
+                                <NavLink 
+                                    to="/notices" 
+                                    className="text-light"
+                                    style={{
+                                        textDecoration: "none",
+                                        padding: "10px"
+                                    }}
+                                    activeStyle={{
+                                        fontWeight: "bold"
+                                    }}
+                                >Notices</NavLink>
+                            </Nav>
+                        </Navbar.Collapse>
                 </Container>
             </Navbar>
         </div>
--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
 import './Home.css';
 
 const Home = () => {
@@ -24,28 +24,40 @@ const Home = () => {
                     </div>
                 </div>
             </div>
-            {/* Preparation Section */}
-            <div className="preparation-container my-5 px-3">
-                <h1>Prepare for IELTS Test</h1>
-                <p>IELTS is the world's most popular English language test. If your aim is to study, migration or work on abroad, you will need to take IELTS test. Our aim is to prepare you for the IELTS test so that you can get a good score. IELTS is accepted by more than 11,000 employers, universities, schools and imigration bodies around the world.</p>
-                <Button variant="dark">Learn More</Button>
+
+            { /* Registration Section */}
+            <div className="registration-section">
+                <div className="registration">IELTS Course Registration</div>
+                <div className="registration">Mock Test Registration</div>
             </div>
+
+            {/* Preparation Section */}
+            <div className="preparation-container bg-dark text-white">
+                <Container className="py-5">
+                    <h1>Prepare for IELTS Test</h1>
+                    <p>IELTS is the world's most popular English language test. If your aim is to study, migration or work on abroad, you will need to take IELTS test. Our aim is to prepare you for the IELTS test so that you can get a good score. IELTS is accepted by more than 11,000 employers, universities, schools and imigration bodies around the world.</p>
+                    <Button variant="light">Learn More</Button>
+                </Container>
+            </div>
+
             {/* Four Services Section */}
-            <div className="services-container my-5">
+            <div className="services-background-container">
                 <h1>Our Main Services</h1>
-                {
-                    services.map(service => service.id < 5? 
-                        <div className="d-flex flex-wrap">
-                            <div className="service-img p-3">
-                                <img src={service.img}/>
+                <div className="services-container">
+                    {
+                        services.map(service => service.id < 5? 
+                            <div className="main-servises-card d-flex flex-wrap rounded-pill">
+                                <div className="service-img p-5">
+                                    <img src={service.img}/>
+                                </div>
+                                <div className="service-info pt-5">
+                                    <h3 className="text-start">{service.title}</h3>
+                                    <p className="text-start text-secondary">{service.description}</p>
+                                </div>
                             </div>
-                            <div className="service-info p-3">
-                                <h3 className="text-start">{service.title}</h3>
-                                <p className="text-start text-secondary">{service.description}</p>
-                            </div>
-                        </div>
-                        : console.log(''))
-                }
+                            : console.log(''))
+                    }
+                </div>
             </div>
         </div>
     );
